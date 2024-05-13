25de19bcb20a26870426ef9d4c249a1878526405
Adam Dubowski <adubowski@deeploy.ml>
2024-05-13 15:51:05 +0200

    Add explainer

--- iris-example.py
+++ iris-example.py
@@ -1,9 +1,15 @@
 import xgboost as xgb
 from sklearn.datasets import load_iris
 import os
+import dill
+from shap import KernelExplainer
+
 
 model_dir = "./model"
 BST_FILE = "model.bst"
+
+exp_dir = "./explainer"
+SHAP_FILE = "explainer.dill"
 
 iris = load_iris()
 y = iris['target']
@@ -12,3 +18,8 @@ xgbclass = xgb.XGBClassifier(max_depth=6)
 xgbclass.fit(X,y)
 model_file = os.path.join((model_dir), BST_FILE)
 xgbclass.save_model(model_file)
+
+explainer = KernelExplainer(xgbclass.predict_proba, X[:100])
+explainer.model = None
+with open(os.path.join(exp_dir, SHAP_FILE), "wb") as f:
+    dill.dump(explainer, f)
