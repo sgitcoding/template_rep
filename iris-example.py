94197aff1d0f15a705cae8a74a6b07f7367767ac
Adam Dubowski <adubowski@deeploy.ml>
2024-05-13 16:56:46 +0200

    Update model to XBG Booster

--- iris-example.py
+++ iris-example.py
@@ -15,12 +15,21 @@ SHAP_FILE = "explainer.dill"
 iris = load_iris()
 y = iris['target']
 X = iris['data']
-xgbclass = xgb.XGBClassifier(max_depth=6)
-xgbclass.fit(X,y)
-model_file = os.path.join((model_dir), BST_FILE)
-xgbclass.save_model(model_file)
+model = xgb.Booster()
 
-predict_fn = lambda x: xgbclass.predict(np.array(x))
+# Train xgboost model
+dtrain = xgb.DMatrix(X, label=y)
+params = {
+    "objective": "multi:softprob",
+    "num_class": 3,
+    "max_depth": 3
+}
+model = xgb.train(params, dtrain, num_boost_round=10)
+
+model_file = os.path.join((model_dir), BST_FILE)
+model.save_model(model_file)
+
+predict_fn = lambda x: model.predict(xgb.DMatrix(np.array(x)))
 explainer = KernelExplainer(predict_fn, X[:100])
 explainer.model = None
 with open(os.path.join(exp_dir, SHAP_FILE), "wb") as f:
